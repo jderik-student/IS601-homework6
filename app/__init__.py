99920dc95da388f0057cf20d4efc0a7bbf51a348
jad553 <jad553@drexel.edu>
2024-06-19 12:55:19 -0400

    Added logging to the application

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,13 @@
     Defines the Application which is a REPL defined to be an interactive calculator
 '''
 
+import logging
+import logging.config
+import os
 import pkgutil
 import importlib
+import sys
+from icecream import ic
 from app.commands import CommandHandler
 from app.commands import Command
 
@@ -14,24 +19,46 @@ class App:
         The Application is a REPL defined to be an interactive calculator
     '''
     def __init__(self): # Constructor
+        os.makedirs('logs', exist_ok=True)
+        self.configure_logging()
         self.command_handler = CommandHandler()
+
+    def configure_logging(self):
+        '''
+            Configures logging
+        '''
+        logging_conf_path = 'logging.conf'
+        if os.path.exists(logging_conf_path):
+            logging.config.fileConfig(logging_conf_path, disable_existing_loggers=False)
+        else:
+            logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info("Logging configured with Level: %s.",logging.root.level)
 
     def load_plugins(self):
         '''
-            # Dynamically load all plugins in the plugins directory
+            Dynamically load all plugins in the plugins directory
         '''
         plugins_package = 'app.plugins'
-        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+        plugins_path = plugins_package.replace('.', '/')
+        if not os.path.exists(plugins_path):
+            logging.warning("Plugins directory '%s' not found.", plugins_path)
+            print("Plugins directory '%s' not found.", plugins_path)
+            sys.exit(0)
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_path]):
             if is_pkg:
                 plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
                 for item_name in dir(plugin_module):
                     item = getattr(plugin_module, item_name)
                     try:
                         if issubclass(item, (Command)) and item is not Command:  # Added extra condition as it was registering the command twice
-                            if plugin_name == "menu":
-                                self.command_handler.register_command(plugin_name, item(self.command_handler))
-                            else:
-                                self.command_handler.register_command(plugin_name, item())
+                            try:
+                                if plugin_name == "menu":
+                                    self.command_handler.register_command(plugin_name, item(self.command_handler))
+                                else:
+                                    self.command_handler.register_command(plugin_name, item())
+                                logging.info("Command %s from plugin %s registered.", plugin_name, plugin_module)
+                            except Exception as e: # pragma: no cover
+                                logging.error("Failed to import plugin %s: %s", plugin_name, ic.format(e))
                     except TypeError:
                         continue  # If item is not a class or unrelated class, just ignore
 
@@ -42,8 +69,15 @@ class App:
         '''
         # Register commands here
         self.load_plugins()
+        logging.info("Application Started")
 
         self.command_handler.list_commands()
         print("Type 'exit' to exit.")
-        while True:  #REPL Read, Evaluate, Print, Loop
-            self.command_handler.execute_command(input(">>> ").split())
+        try:
+            while True:  #REPL Read, Evaluate, Print, Loop
+                self.command_handler.execute_command(input(">>> ").split())
+        except KeyboardInterrupt:
+            logging.info("Application interrupted and exiting gracefully.")
+            sys.exit(0)
+        finally:
+            logging.info("Application shutdown.")
